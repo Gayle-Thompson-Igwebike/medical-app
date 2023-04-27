0c1a1989dc59f4ce886ba1655638b81f3aa8379b
Andriana <andrianasaffo@gmail.com>
2023-04-27 15:29:41 +0100

    Header for page completed

---
+++ src/Header.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import NavBar from "./NavBar";
+
+function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => setShowMenu((s) => !s);
+
+  const handleClose = () => setShowMenu(false);
+
+  return (
+    <header>
+      {!showMenu && (
+        <aside className="menu">
+          <aside className="login">
+            <FontAwesomeIcon
+              icon={faCircleUser}
+              style={{ color: "#b6d1d9", width: "2em", height: "2em" }}
+            />
+            <a href="/login">Log In</a>
+          </aside>
+          <button onClick={toggleMenu} className="menu-btn">
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+        </aside>
+      )}
+      {showMenu && <NavBar showMenu={showMenu} handleClose={handleClose} />}
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/NavBar.jsx
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
+function NavBar(props) {
+  return (
+    <nav showMenu={props.showMenu} className="navbar">
+      <section className="btn">
+        <button onClick={props.handleClose} className="close-btn">
+          <FontAwesomeIcon icon={faXmark} />
+        </button>
+      </section>
+      <section className="menu-items">
+        <a href="/home" className="list-item">
+          Home
+        </a>
+        <a href="/about" className="list-item">
+          About Us
+        </a>
+        <a href="/team" className="list-item">
+          Meet the Team
+        </a>
+        <a href="/testimonials" className="list-item">
+          Testimonials
+        </a>
+        <a href="/contact" className="list-item">
+          Contact Us
+        </a>
+      </section>
+    </nav>
+  );
+}
+
+export default NavBar;
