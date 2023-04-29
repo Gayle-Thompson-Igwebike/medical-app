a3fe33751d4ddf6c465747d2295ebff4294ae3dc
Karleen Richards <karleenmsrichards@gmail.com>
2023-04-29 11:27:00 +0100

    completed hero section mobile and desktop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import { HomeHero } from "./HomeHero";
 
 function App() {
   return (
     <div className="App">
-      
+      <HomeHero />
     </div>
   );
 }
