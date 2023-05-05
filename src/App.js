584712c61eb7112e4d9f9972b3e54a93a7714161
KhadijaHaji <108950237+KhadijaHaji@users.noreply.github.com>
2023-05-05 21:09:22 +0100

    carousel completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-
+import HomeTestimonialCarousel from "./HomeTestimonialCarousel";
 
 
 function App() {
   return (
     <div className="App">
-   
+      <HomeTestimonialCarousel />
     </div>
   );
 }
