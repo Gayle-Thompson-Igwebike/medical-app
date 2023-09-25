81b4e1ac20d67ba8d7cb8b94f27be59f79570041
Andriana <andrianasaffo@gmail.com>
2023-09-25 10:42:29 +0100

    Update App.js
    Update what shows on the home screen

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import React from "react";
 import "./App.css";
 import { HomeHero } from "./HomeHero";
+import HomeTestimonialCarousel from "./HomeTestimonialCarousel"
 
 function App() {
   return (
     <div className="App">
       <HomeHero />
+      <HomeTestimonialCarousel/>
 
     </div>
   );
