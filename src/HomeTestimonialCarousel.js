1b1e6eaac40ad8734e7291dca6daaa974605a0ee
Andriana <andrianasaffo@gmail.com>
2023-09-28 12:14:28 +0100

    Update HomeTestimonialCarousel.js
    Edit code to eliminate the use oof useRef. setInterval input to make testimonials move independently, buttons the manually move testimonials. Slight edits made to button functions

--- src/HomeTestimonialCarousel.js
+++ src/HomeTestimonialCarousel.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import "./HomeTestimonial.css";
 import quoteLeft from "./quote-left.png";
 import quoteRight from "./quote-right.png";
@@ -26,64 +26,58 @@ const homeTestimonials = [
 
 function HomeTestimonialCarousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const carouselWrapperRef = useRef();
 
   useEffect(() => {
-    carouselWrapperRef.current.children[currentIndex].scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  }, [currentIndex]);
-
-  // useEffect(() => {
-  //   const element = carouselWrapperRef.current.children[currentIndex];
-  //   element.scrollIntoView({ behavior: "smooth", block: "start" });
-  // }, [currentIndex]);
+    const scrollInterval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % homeTestimonials.length);
+    }, 5000);
+    return () => {
+      clearInterval(scrollInterval);
+    };
+  }, []);
 
   const nextTestimonial = () => {
-    setCurrentIndex((currentIndex + 1) % homeTestimonials.length);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % homeTestimonials.length);
   };
 
   const prevTestimonial = () => {
-    setCurrentIndex(
-      currentIndex === 0 ? homeTestimonials.length - 1 : currentIndex - 1
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? homeTestimonials.length - 1 : prevIndex - 1
     );
   };
 
   return (
     <div className="testimonial-carousel">
-      <div className="testimonial ">
-        <div className="carousel-wrapper" ref={carouselWrapperRef}>
-          {homeTestimonials.map((testimonial, index) => (
-            <div className="carousel-card-wrapper" key={index}>
-              <img src={quoteLeft} alt="quote-left" className="quotes" />
-              <div className="quote-container">
-                <p className="quote">{testimonial.quote}</p>
-              </div>
-              <img src={quoteRight} alt="quote-right" className="quotes" />
-              <p className="name-age">
-                {testimonial.name} {testimonial.age}
-              </p>
+      <div className="testimonial">
+        <div className="carousel-wrapper">
+          <div className="carousel-card-wrapper">
+            <img src={quoteLeft} alt="quote-left" className="quotes" />
+            <div className="quote-container">
+              <p className="quote">{homeTestimonials[currentIndex].quote}</p>
             </div>
-          ))}
+            <img src={quoteRight} alt="quote-right" className="quotes" />
+          </div>
+          <p className="name-age">
+            {homeTestimonials[currentIndex].name}{" "}
+            {homeTestimonials[currentIndex].age}
+          </p>
         </div>
-
-        <div className="before-after">
-          <button className="before-button" onClick={prevTestimonial}>
-            <FontAwesomeIcon
-              icon={faCircle}
-              size="2xs"
-              style={{ color: "#ffffff" }}
-            />
-          </button>
-          <button className="after-button" onClick={nextTestimonial}>
-            <FontAwesomeIcon
-              icon={faCircle}
-              size="2xs"
-              style={{ color: "#ffffff" }}
-            />
-          </button>
-        </div>
+      </div>
+      <div className="before-after">
+        <button className="before-button" onClick={prevTestimonial}>
+          <FontAwesomeIcon
+            icon={faCircle}
+            size="2xs"
+            style={{ color: "#ffffff" }}
+          />
+        </button>
+        <button className="after-button" onClick={nextTestimonial}>
+          <FontAwesomeIcon
+            icon={faCircle}
+            size="2xs"
+            style={{ color: "#ffffff" }}
+          />
+        </button>
       </div>
     </div>
   );
