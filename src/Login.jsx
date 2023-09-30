82aed1891710444e2815d1262f6f85b111a319da
Andriana <andrianasaffo@gmail.com>
2023-09-30 10:51:59 +0100

    Update Login.jsx
    Input skeleton of login form

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const initialState = {
+    email: "",
+    password: "",
+  };
+  const [formData, setFormData] = useState(initialState);
 
-export default Login
+  function handleChange(event) {
+    event.preventDefault();
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  }
+
+  return (
+    <div>
+      <form>
+        <h1>Log In</h1>
+        <label htmlFor="email">Enter your email</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="myname@emailprovider.com"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="pwd">Enter your password</label>
+        <input
+          id="pwd"
+          type="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
